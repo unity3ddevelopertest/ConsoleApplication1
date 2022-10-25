4f3040699bc69d554e0bfce462a392cb31c6f0c3
Administrator <unity3ddevelopertest@gmail.com>
2022-10-25 19:10:21 +0300

    First

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+        
+        
+        }
+    }
+}
